9d2bccf8d4677fefc384ec88f579ddf7d20a4770
jj2delgado <jj2delgado@yahoo.com>
2019-10-14 01:12:38 -0600

    finished header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,114 @@
 import React, {Component} from 'react'
+import './Header.css'
 
 class Header extends Component{
     render(){
         return(
-            <div>
-                <h1>Test</h1>
+            <div className='Header-Container'>
+                <div className='black-bar'>
+
+                    <span>
+                    <i class="fas fa-map-marker-alt" style={{color:'white'}}></i>
+                    <h1>Location Services Coming Soon</h1>
+                    </span>
+
+                    <span>
+                    <i class="fas fa-clipboard-list" style={{color:'white'}}></i>
+                    <h1>Check an Order</h1>
+                    </span>
+
+                    <span>
+                    <i class="far fa-question-circle" style={{color:'white'}}></i>
+                    <h1>Help</h1>
+                    </span>
+
+                    <div className='black-filler'/>
+                </div>
+                <div className='Header-Top'>
+                    <img src="https://www.gamestop.com/on/demandware.static/Sites-gamestop-us-Site/-/default/dwb103fa93/images/svg-icons/logo-1280.svg"/>
+                        <div>
+                        <span className='Search-Bar'>
+                         <i class="fas fa-search"></i>
+                         <input placeholder='Find games, consoles, tablets, and more' className='Header-Search-Bar'/>
+                         <button className='Header-Search'>SEARCH</button>
+                         </span>
+                        </div>
+                    <div className='Header-Links-Top-Container'>
+                        <span>
+                        <i class="fas fa-sync-alt" style={{color:'red'}}></i>
+                        <h6>Trade-Ins</h6>
+                        </span>
+
+                        <span>
+                        <i class="fas fa-power-off"></i>
+                        <h6>PowerUp Rewards</h6>
+                        </span>
+
+                        <span>
+                        <i class="far fa-user"></i>
+                        <h6>Account</h6>
+                        </span>
+
+                        <span id="Last-Link">
+                        <i class="fas fa-shopping-cart"></i>
+                        <h6>Cart</h6>
+                        </span>
+                    </div>
+                    
+                </div>
+
+                <div className='Header-Bottom-Wrapper'>
+                    <div className='Header-Bottom'>
+                        <div>
+                        <h1>VIDEO GAMES</h1>
+                        </div>
+
+                        <span></span>
+
+                        <div>
+                        <h1>ACCESSORIES</h1>
+                        </div>
+
+                        <span></span>
+
+                        <div>
+                        <h1>TOYS & COLLECTIBLES</h1>
+                        </div>
+
+                        <span></span>
+
+                        <div>
+                        <h1>ELECTRONICS</h1>
+                        </div>
+
+                        <span></span>
+
+                        <div>
+                        <h1>CLOTHING</h1>
+                        </div>
+
+                        <span></span>
+
+                        <div>
+                        <h1>DEALS</h1>
+                        </div>
+
+                        <span></span>
+
+                        <div>
+                        <h1>WEEKLY AD</h1>
+                        </div>
+
+                        <span></span>
+
+                        <div>
+                        <h1>GIFT CARDS</h1>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
         )
     }
 }
+export default Header
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,97 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import Header from '../Header/Header'
 
 class Home extends Component{
     render(){
         return(
-            <div>
+            <div className='main-home-container'>
                 <Header />
+                <div id="MyCarousel" class="carousel slide" data-ride="carousel">
+                    <ol class='carousel-indicators'>
+                        <li data-target='#MyCarousel' data-slide-to='0' class='active'></li>
+                        <li data-target='#MyCarousel' data-slide-to='1'></li>
+                        <li data-target='#MyCarousel' data-slide-to='2'></li>
+                        <li data-target='#MyCarousel' data-slide-to='3'></li>
+                        <li data-target='#MyCarousel' data-slide-to='4'></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class='carousel-item active'>
+                            <img class='d-block w-100' src='https://media.playstation.com/is/image/SCEA/tom-clancys-ghost-recon-breakpoint-hero-banner-01-ps4-us-03may19?$native_nt$' alt='First Slide'/>
+                            <div class='carousel-caption'>
+                                    <h3>Test</h3>
+                                    <p>(first slide)</p>
+                                </div>
+                        </div>
+                        <div class='carousel-item'>
+                            <img class='d-block w-100' src='https://i2.wp.com/metro.co.uk/wp-content/uploads/2019/04/the-last-of-us-part-2-4406.jpg?quality=90&strip=all&zoom=1&resize=644%2C362&ssl=1' alt='Second Slide'/>
+                            <div class='carousel-caption'>
+                                <h3>Test</h3>
+                                <p>(second slide)</p>
+                            </div>
+                        </div>
+                        <div class='carousel-item'>
+                            <img class='d-block w-100' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWYn4o_Z_1ruXPUKEN-qA6SGmqJyUhmJ6jQNL6SayFHG_SB7gH' alt='Third Slide'/>
+                            <div class='carousel-caption'>
+                                <h3>Test</h3>
+                                <p>(third slide)</p>
+                            </div>
+                        </div>
+                        <div class='carousel-item'>
+                            <img class='d-block w-100' src='https://media.gamestop.com/i/gamestop/ForceFriday_Logo/triple-force-friday-star-wars.png' alt='Fourth Slide'/>
+                            <div class='carousel-caption'>
+                                <h3>Test</h3>
+                                <p>(fourth slide)</p>
+                            </div>
+                        </div>
+                        <div class='carousel-item'>
+                            <img class='d-block w-100' src='https://cdn2.unrealengine.com/Diesel%2Fproduct%2Fmint%2Fhome%2FGhostbustersRemastered_GamePagePromo-1920x1080-60c14b012afd9440f08a5d7e91fa11101df91630.jpg' alt='Fifth Slide'/>
+                            <div class='carousel-caption'>
+                                <h3>Test</h3>
+                                <p>(fifth slide)</p>
+                            </div>
+                        </div>
+                        <a class='carousel-control-prev' href='#MyCarousel' role='button' data-slide='prev'>
+                            <span class='carousel-control-prev-icon'></span>
+                            <span class='sr-only'>Previous</span>
+                        </a>
+                        <a class='carousel-control-next' href='#MyCarousel' role='button' data-slide='next'>
+                            <span class='carousel-control-next-icon'></span>
+                            <span class='sr-only'>Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div className='Lower-Home-Container'>
+                    <h1>FEATURED PRODUCTS & OFFERS</h1>
+                </div>
+                <div>
+                    <h1>SAVE BIG WITH PRE-OWNED</h1>
+                </div>
+                <div>
+                    <h1>TOYS & COLLECTIBLES</h1>
+                </div>
+                <div>
+                    <h1>RECENTLY RELEASED VIDEO GAMES</h1>
+                    <button>
+                        <Link to='/test'>
+                            VIEW ALL RECENTLY RELEASED VIDEO GAMES
+                        </Link>
+                    </button>
+                </div>
+                <div>
+                    <h1>VIDEO GAMES COMING SOON</h1>
+                    <button>
+                        <Link>
+                            VIEW ALL VIDEO GAMES COMING SOON
+                        </Link>
+                    </button>
+                </div>
+                <div>
+                    <h1>TOP CATEGORIES</h1>
+                </div>
+                <div>
+                    <h1>RECOMMENDED FOR YOU</h1>
+                </div>
             </div>
         )
     }
