8b069ed48f941845f1f328707e945e8b72ce1c2b
jj2delgado <jj2delgado@yahoo.com>
2019-11-01 14:32:06 -0600

    Home Page Finished CSS

--- src/Components/Sub-Home/Sub-Home.js
+++ src/Components/Sub-Home/Sub-Home.js
@@ -8,6 +8,12 @@ class SubHome extends Component{
             <div className='Lower-Home-Container'>
                 <div>
                 <h1>FEATURED PRODUCTS & OFFERS</h1>
+                <span className="Featured-Container">
+                    <img src="https://cdn.images.express.co.uk/img/dynamic/143/590x/Luigi-s-Mansion-3-on-Nintendo-Switch-1198156.jpg?r=1572554132295" />
+                    <img src='https://store-images.s-microsoft.com/image/apps.1926.68332208580498659.9222b095-8101-4531-b386-33547ee9944f.9fe84d5d-ef45-4807-98c3-d0978ab24399?mode=scale&q=90&h=1080&w=1920&background=%23FFFFFF'/>
+                    <img src='https://cdn.wccftech.com/wp-content/uploads/2019/05/WCCFghostreconbreakpoint1.jpg'/>
+                    <img src='https://media.gamestop.com/i/gamestop/11051548/The-Nightmare-Before-Christmas-Series-3-Blind-Box-Domez-Only-at-GameStop'/>
+                </span>
                 </div>
                 
                 <div>
